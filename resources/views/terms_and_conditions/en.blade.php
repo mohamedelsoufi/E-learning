cb68ac212f2cac3f76345617393116c43c4de0f1
ahmedmaher179200 <73036767+ahmedmaher179200@users.noreply.github.com>
2022-05-29 09:52:44 +0200

    last

---
+++ resources/views/terms_and_conditions/en.blade.php
@@ -0,0 +1,77 @@
+<div style="margin-left: 15px; margin-right: 15px;">
+    <div style="text-align: center;">
+        <h1 >Terms and Conditions</h2>
+        <p>Please read the terms and conditions of the use stipulated accurately before using this site.</p>
+        <p>Through these conditions, we explain how to deal according to the binding rules for using our site</p>
+    </div>
+
+    <h2>A-privacy Policy</h1>
+        <div>The privacy policy governs the way our platform collects, uses, maintains and discloses information collected from users (teachers and students) and the privacy policy applies to all products and services offered by the platform and the purpose of using this information is to improve the level of service The Services constitute acceptance of the Privacy Policy</div>
+        <div style="margin: 25px;">
+            <ui>
+                <li style="margin-top: 20px; margin-bottom: 10px; font-weight: bold;">Personally Identifiable Information: Personal Information</li>
+                    <div style="margin-left: 25px;margin-right: 25px;">
+                        <div>We may collect personally identifiable information from Users in a variety of ways, including, but not limited to, when Users visit and log on to our Platform, and in connection with other activities, services, features or resources we make available on the Platform. Users may be asked, as the case may be, for name, email address and credit card information. We do not collect personally identifiable information from Users unless they voluntarily provide such information to us. Users can always refuse to provide personally identifiable information, which would prevent them from participating in certain Platform-related activities.</div>
+                    </div>
+                <li style="margin-top: 20px;margin-bottom: 10px;font-weight: bold;">Non-Personally Identifiable Information: Usage Data</li>
+                    <div style="margin-left: 25px;margin-right: 40px;">
+                        <div>Agreeing to the Terms and Conditions means agreeing to share certain personal and entered information; Including access to the quotas and our authority to amend the data.
+                            We may collect non-personally identifiable information about Users whenever they interact with the Platform. Non-personally identifiable information may include browser name, type of computer and technical information about Users connection to the Platform, such as the operating system, Internet service providers and other similar informationHow we use the information collected</div>
+                    </div>
+                <li style="margin-top: 20px;margin-bottom: 10px;font-weight: bold;">How we use the information collected</li>
+                <div style="margin-left: 25px;margin-right: 40px;">
+                    <div>Our website may collect and use users personal information for the following purposes</div>
+                </div>
+                <div style="margin-left: 60px;margin-right: 40px;">
+                    <div>
+                        <div style="margin-bottom: 7px;">− Improving customer service: The information collected helps us respond to customer requests and support needs more effectively.</div>
+                        <div style="margin-bottom: 7px;">− To personalize your user experience, we may use information in the aggregate to understand how our users as a group use the services and resources available on the Site.</div>
+                        <div style="margin-bottom: 7px;">− Platform improvement: We may use the information collected to improve our products and services.</div>
+                        <div style="margin-bottom: 7px;">− Third Party Information Sharing: We may use the information users provide about themselves when placing an order only to provide the service for that request. We do not share this information with third parties except to the extent necessary to provide the service.</div>
+                        <div style="margin-bottom: 7px;">− conduct a promotion, contest, survey, or other feature of the Application: to send information to users that they agree to receive about topics we think are of interest to them. To direct periodic emails to them We may use the email address to send User information and updates relating to their order. It may also be used to respond to their inquiries, questions and/or other requests.</div>
+                    </div>
+                </div>
+            </ui>
+        </div>
+    <h2>B-Sharing user information</h1>
+        <div style="margin-left: 25px;margin-right: 40px;margin-bottom: 15px;">
+            <div>We do not sell, trade or rent personally identifiable information to other users. We may share generic aggregated demographic information that is not linked to any personally identifiable information relating to users, with our business partners, trusted affiliates and advertisers for the purposes described above. We may use third party service providers to help us operate the Site or administer activities on our behalf, such as sending out newsletters or conducting surveys. We may share user information with these parties for those limited purposes</div>
+        </div>
+        <div style="margin-left: 60px;margin-right: 40px;">
+            <div>
+                <div style="margin-bottom: 7px;">- User Information Others Provide: We may receive information that other people provide to us, some of which may be about you. For example, when other users you know use our Service, they may provide us with your phone number from their mobile address book (just as you may provide information about them), or they may direct a message to you or direct messages to groups you belong to.</div>
+            </div>
+        </div>
+
+
+
+    <h2>C-lessons</h1>
+    <div style="margin-left: 60px;margin-right: 40px;">
+        <div>
+            <div style="margin-bottom: 7px;color: blue;">− We have the right to access data and video and audio calls to ensure the right of all parties.</div>
+            <div style="margin-bottom: 7px;color: blue;">− Being present and adhering to the timings of the classes and avoiding any interruptions, conversations or side actions that lead to deviation from the context and purpose of the class.</div>
+        </div>
+    </div>
+
+
+    <h2>D-Communication and Violations</h1>
+        <div style="margin-left: 60px;margin-right: 40px;">
+            <div>
+                <div style="margin-bottom: 7px;color: blue;">− Not to use invalid or offensive words, and if this is proven, the platform has the right to debit the violating person’s wallet and delete the account.</div>
+                <div style="margin-bottom: 7px;color: blue;">- It is forbidden to publish any offensive, racist or inciting to violence publications.</div>
+                <div style="margin-bottom: 7px;color: blue;">− It is not allowed to record classes and send them to students without prior permission from the platform.</div>
+                <div style="margin-bottom: 7px;color: blue;">− Avoid taking any personal data from both parties, whether by phone number or any social media</div>
+            </div>
+        </div>
+
+    <h2>E-To compensate</h1>
+        <div style="margin-left: 25px;margin-right: 40px;margin-bottom: 15px;">
+            <div>User agrees not to claim any compensation and not hold us liable for any and all damages, losses and expenses of any kind (including reasonable legal fees and costs) related to, arising out of or in any way in connection with any of the following: (a) User access to our Service or its use, including information provided in connection with it; (b) the user breach of the Terms of Service; or (c) any misrepresentation made by the user. The user is obligated to cooperate fully with us in the defense or settlement of any claim.</div>
+        </div>
+
+    <h2>F-Waiver of Liability</h1>
+    <div style="margin-left: 25px;margin-right: 40px;margin-bottom: 15px;">
+        <div style="margin-bottom: 7px;color: blue;">The user is obligated not to use the service for any illegal or unauthorized purpose. It undertakes not to interfere with or disrupt any other user s experience. We are not responsible for any user interaction in the “communication” feature on the site, any violation of these terms may lead to financial penalties or account deletion.
+            The platform is not responsible for any data damage, loss of account, direct or indirect damage; But we seek to secure you by all available and possible means, according to what has been agreed upon in these announced conditions.</div>
+    </div>
+</div>
